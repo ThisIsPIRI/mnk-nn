2f96359ba1392879dc5c81d2a3b0a3c06c62ee31
ThisIsPIRI <thisispiri@gmail.com>
2019-01-30 05:57:35 +0900

    Add AIs to test against and do so

---
+++ mnkais.py
@@ -0,0 +1,19 @@
+import random
+
+from mnk import Shape
+
+class RandomAi:
+	def play(self, game):
+		candidates = []
+		for i in range(len(game.array)):
+			for j in range(len(game.array[i])):
+				if game.array[i][j] == Shape.N:
+					candidates.append((j, i))
+		return random.choice(candidates)
+
+class FillerAi:
+	def play(self, game):
+		for i in range(len(game.array)):
+			for j in range(len(game.array)):
+				if game.array[i][j] == Shape.N:
+					return j, i
--- polgrad_main.py
+++ polgrad_main.py
@@ -1,11 +1,32 @@
 import tensorflow as tf
 
+from mnk import MnkGame
+from mnkutil import needs_session
 from polgrad_tf import PolgradRunnerTf
+from mnkais import FillerAi, RandomAi
+
+@needs_session
+def testAgainst(runner, ai, game=None, session=None):
+	if game is None:
+		game = MnkGame(3, 3)
+	for i in range(game.horSize * game.verSize):
+		if i % 2 == 0:
+			game.place(ai.play(game))
+		else:
+			game.place(runner.play(game, session=session))
+	if len(game.history) == 9:
+		print("draw")
+	elif len(game.history) % 2 == 0:
+		print("O won")
+	else:
+		print("X won")
 
 def main():
-	LOAD = True
+	LOAD = False
 	WEIGHT_PATH = "weight/weight.ckpt"
 	DIMEN = (3, 3); BOARD_SIZE = DIMEN[0] * DIMEN[1]
+
+	training_ordered = input("train the network?(y/n): ") == "y"
 
 	runner = PolgradRunnerTf([BOARD_SIZE, BOARD_SIZE * 2, 20, BOARD_SIZE], [tf.nn.relu, tf.nn.relu, tf.nn.softmax])
 	saver = tf.train.Saver()
@@ -14,7 +35,10 @@ def main():
 			tf.train.Saver().restore(sess, WEIGHT_PATH)
 		else:
 			sess.run(tf.global_variables_initializer())
-		runner.train(dimen=DIMEN, winLen=3, batch_size=100, cycles=100, session=sess)
+		if training_ordered:
+			runner.train(dimen=DIMEN, winLen=3, batch_size=100, cycles=100, session=sess)
+		for i in range(100):
+			testAgainst(runner, FillerAi(), session=sess)
 		saver.save(sess, WEIGHT_PATH)
 
 if __name__ == "__main__":
